db49d015f1e09d6816b4ee530b4eb9556a9179df
MilkyRamen <127574535+MilkyRamen@users.noreply.github.com>
2023-06-11 19:23:10 -0400

    add seeds.sql and fill out tables

---
+++ seeds.sql
@@ -0,0 +1,37 @@
+INSERT INTO departments (name, id)
+VALUES ("Finance", 1),
+       ("Sales", 2),
+       ("Human Resources", 3),
+       ("Engineering", 4),
+       ("Technology", 5);
+
+INSERT INTO roles (name, id)
+VALUES ("CEO", 1),
+       ("Project Manager", 2),
+       ("Chief Marketing Officer", 3),
+       ("Vice President", 4),
+       ("Secretary", 5),
+       ("Account Manager", 6),
+       ("CFO", 7);
+
+INSERT INTO employees (name, id, active)
+VALUES ("Laure Sanjeet", 1, true),
+       ("Enos Sudenaz", 2, true),
+       ("Sandile Evyatar", 3, true),
+       ("Padma Eriks", 4, false),
+       ("Yorgos Parth", 5, true),
+       ("Sadia Mitul", 6, true),
+       ("Edith Yolotzin", 7, true),
+       ("Callisto Bruno", 8, true),
+       ("Maider Ezras", 9, false),
+       ("Milan Heino", 10, true),
+       ("Gabrielle Uttara", 11, true),
+       ("Adeline Liba", 12, true),
+       ("Hilal Viktar", 13, true),
+       ("Alban Bellona", 14, false),
+       ("Mohammed Teresa", 15, true),
+       ("Meshach Rafahel", 16, true),
+       ("Branko Eneko", 17, false),
+       ("Kaltrina Neon", 18, true),
+       ("Zhanna Noa", 19, true),
+       ("Kimmie Anah", 20, false); 
--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const mysql = require('mysql2');
 
-const port = process.env.PORT || 4444;
+const PORT = process.env.PORT || 3306;
 const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// db connection
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'luhroot',
+        database: 'company_db'
+    },
+    console.log(`connected to company_db`)
+);
+
+db.query('SELECT * FROM departments', function (err, results){
+    console.log(results);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
